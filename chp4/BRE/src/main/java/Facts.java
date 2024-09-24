75947cb197b9da6c96d6a623f6510f571ce046e9
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-09-24 09:01:45 -0300

    load facts from json

--- chp4/BRE/src/main/java/Facts.java
+++ chp4/BRE/src/main/java/Facts.java
@@ -1,7 +1,19 @@
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Facts {
     private final HashMap<String, String> facts = new HashMap<>();
+    private final JSONParser parser = new JSONParser();
 
     public String getFact(String key){
         return this.facts.get(key);
@@ -9,5 +21,14 @@ public class Facts {
 
     public void addFact(String key, String value) {
         this.facts.put(key, value);
+    }
+
+    public void loadFacts(File file) throws IOException, ParseException {
+        JSONObject jsonObject = (JSONObject) parser.parse(new FileReader(file));
+
+        for (Object entry : jsonObject.entrySet()) {
+            Map.Entry<String, String> row = (Map.Entry<String, String>) entry;
+            this.addFact(row.getKey(), row.getValue());
+        }
     }
 }
--- chp4/BRE/src/test/java/BusinessRuleEngineTest.java
+++ chp4/BRE/src/test/java/BusinessRuleEngineTest.java
@@ -1,5 +1,10 @@
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.mockito.Mockito.*;
 
 public class BusinessRuleEngineTest {
@@ -32,5 +37,15 @@ public class BusinessRuleEngineTest {
         businessRuleEngine.addRule(mockAction);
         businessRuleEngine.run();
         verify(mockAction).perform(mockFacts);
+    }
+
+    @Test
+    public void loadFactsFromJson() throws IOException, ParseException {
+        final File file = new File("C:\\Users\\Julio Murta\\source\\repos\\java-stuff\\chp4\\BRE\\src\\main\\resources\\facts.json");
+        final Facts facts = new Facts();
+        facts.loadFacts(file);
+        Assert.assertEquals("X", facts.getFact("A"));
+        Assert.assertEquals("Y", facts.getFact("B"));
+        Assert.assertEquals("Z", facts.getFact("C"));
     }
 }
